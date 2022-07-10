fd0d53a9f4797a050311a6682f999f3635cbe9ff
manuelcampagnolo <mlc@isa.ulisboa.pt>
2022-07-10 15:33:20 +0100

    Update reproject_sinusoidal_to_wgs84.py
    Reproject MODIS sinusoidal coordinates to lat/lon wgs84

--- reproject_sinusoidal_to_wgs84.py
+++ reproject_sinusoidal_to_wgs84.py
@@ -3,16 +3,15 @@ import string # generate randomm string
 import random
 from haversine import haversine, Unit, inverse_haversine, Direction
 
-# a,b are compulsory; otherwise the projection is not well positioned
+# proj description of MODIS sinusoidal projectio; a,b are compulsory; otherwise the projection is not well positioned
 crs_sinu='+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +a=6371007.181 +b=6371007.181 +no_defs'
 
-# fn csv input file
-# output layer name
-# X,Y are the names of the columns with coordinates in the file
+# fn: csv input file with path
+# ln: output layer name
+# X,Y are the names of the columns with coordinates in fn 
 # inputCRSTYPE can be 'proj4' or 'epsg'
 # inputCRSTYPE is a string if 'proj4' and an integer if 'epsg'
 def create_4326_layer_from_lat_lon_txt_csv(fn,ln,X,Y,inputCRSTYPE,inputCRS,sep=","):
-    #auxfn=os.path.splitext(fn)[0]+'.gpkg'
     params='?delimiter=%s&xField=%s&yField=%s' % (sep, X, Y)
     uri='file:///'+fn+params
     mylayer = QgsVectorLayer(uri, '', "delimitedtext")
@@ -24,10 +23,11 @@ def create_4326_layer_from_lat_lon_txt_csv(fn,ln,X,Y,inputCRSTYPE,inputCRS,sep="
     save_options = QgsVectorFileWriter.SaveVectorOptions()
     transform_context = QgsProject.instance().transformContext()
     # Write to GeoPackage (default)
+    # instead of simply to os.path.splitext(fn)[0]+'.gpkg'
     letters = string.ascii_lowercase
     lixo=(''.join(random.choice(letters) for i in range(10)) )
     error = QgsVectorFileWriter.writeAsVectorFormatV3(mylayer, os.path.join(myfolder,lixo+'.gpkg'),transform_context,save_options)
-    mylayer=QgsVectorLayer(os.path.join(myfolder,lixo+'.gpkg'),ln,"ogr") # added fid column
+    mylayer=QgsVectorLayer(os.path.join(myfolder,lixo+'.gpkg'),ln,"ogr") # adds fid column
     # reproject if necessary
     if not inputCRS==4326:
         if inputCRSTYPE=='proj4':
