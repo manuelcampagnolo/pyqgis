338c09bc0ad2a40bb232ca0e54a61607688704d8
manuelcampagnolo <mlc@isa.ulisboa.pt>
2022-07-10 16:22:40 +0100

    Update dissolve.py

--- vector/processing_run/dissolve.py
+++ vector/processing_run/dissolve.py
@@ -3,6 +3,10 @@ dict_params={'DISSOLVE' : True, 'DISTANCE' : dist_buf}
 mylayer=my_processing_run("native:buffer",ln_isocronas,dict_params,ln_isocronas_buf)
 my_remove_layer(ln_isocronas)
 
+# multiple attributes
+dict_params={'FIELD': [an_day_int,'instrument']}
+mylayer=my_processing_run("native:dissolve",ln_new,dict_params,ln_diss)
+
 ##################################################################################### dissolve with attribute distance
 property_distance=QgsProperty.fromExpression('"Distance"')
 # Create buffer around Roads
