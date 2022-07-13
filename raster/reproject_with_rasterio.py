afe6538c15391530db01ee7a88ca33f43f997ca7
manuelcampagnolo <mlc@isa.ulisboa.pt>
2022-07-13 13:48:51 +0100

    Create reproject_with_rasterio.py

---
+++ raster/reproject_with_rasterio.py
@@ -0,0 +1,41 @@
+# from https://pygis.io/docs/e_raster_reproject.html
+#Reprojecting a Raster with Rasterio
+
+dst_crs = "EPSG:3857"  # web mercator(ie google maps)
+
+with rasterio.open("../data/LC08_L1TP_224078_20200518_20200518_01_RT.TIF") as src:
+    src_transform = src.transform
+
+    # calculate the transform matrix for the output
+    dst_transform, width, height = calculate_default_transform(
+        src.crs,
+        dst_crs,
+        src.width,
+        src.height,
+        *src.bounds,  # unpacks outer boundaries (left, bottom, right, top)
+    )
+
+    # set properties for output
+    dst_kwargs = src.meta.copy()
+    dst_kwargs.update(
+        {
+            "crs": dst_crs,
+            "transform": dst_transform,
+            "width": width,
+            "height": height,
+            "nodata": 0,  # replace 0 with np.nan
+        }
+    )
+
+    with rasterio.open("../temp/LC08_20200518_webMC.tif", "w", **dst_kwargs) as dst:
+        # iterate through bands
+        for i in range(1, src.count + 1):
+            reproject(
+                source=rasterio.band(src, i),
+                destination=rasterio.band(dst, i),
+                src_transform=src.transform,
+                src_crs=src.crs,
+                dst_transform=dst_transform,
+                dst_crs=dst_crs,
+                resampling=Resampling.nearest,
+            )
