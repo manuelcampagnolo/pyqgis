8eff6072705ea5ffc3592e83cad5a0e00005238d
manuelcampagnolo <mlc@isa.ulisboa.pt>
2022-08-15 15:29:57 +0100

    Create source_auxiliary_functions.py

---
+++ source_auxiliary_functions.py
@@ -0,0 +1,2 @@
+myscriptsfolder=r'C:\Users\mlc\OneDrive - Universidade de Lisboa\Documents\Aulas-Cursos\scripts_python'
+exec(open(os.path.join(myscriptsfolder,'auxiliary_functions.py').encode('utf-8')).read())
