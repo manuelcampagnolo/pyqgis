2203c7014eb50d8969309bdc0a2e628d36533272
manuelcampagnolo <mlc@isa.ulisboa.pt>
2022-08-16 17:12:27 +0100

    Update reproject_with_rasterio.py

--- raster/reproject_with_rasterio.py
+++ raster/reproject_with_rasterio.py
@@ -1,6 +1,10 @@
 # from https://pygis.io/docs/e_raster_reproject.html
 #Reprojecting a Raster with Rasterio
 
+from rasterio.warp import calculate_default_transform
+
+# rasterio.warp.calculate_default_transform(src_crs, dst_crs, width, height, left=None, bottom=None, right=None, top=None, gcps=None, rpcs=None, resolution=None, dst_width=None, dst_height=None, **kwargs)
+
 dst_crs = "EPSG:3857"  # web mercator(ie google maps)
 
 with rasterio.open("../data/LC08_L1TP_224078_20200518_20200518_01_RT.TIF") as src:
